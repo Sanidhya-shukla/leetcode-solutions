c57a63e2431340aac4f9ff42d005ca5eb3d4ed58
Sanidhya-shukla <sanidhyashantanushukla@gmail.com>
2026-03-24 13:09:46 +0530

    Time: 404 ms (40%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-select patient_id, patient_name, conditions from patients
-where conditions REGEXP '(^| )DIAB1';
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%'
+   OR conditions LIKE '% DIAB1%';
